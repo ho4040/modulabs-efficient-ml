cdcd68363ae0d78ec5208cb31c8b5a78f2efe703
rick <rick@caveduck.io>
2024-02-17 12:25:36 +0900

    QAT 예제 코드 추가

--- quantization_aware_traning/qat.py
+++ quantization_aware_traning/qat.py
@@ -5,42 +5,54 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from pathlib import Path
-import numpy as np
-import matplotlib.pyplot as plt
 import os
+import matplotlib.pyplot as plt
 # %%
-# 랜덤시드 고정
 _ = torch.manual_seed(0)
-#%%
-# MNIST 데이터셋을 불러와 사용할 수 있도록 준비
+# %%
+# MNIST dataset 준비
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 train_loader = torch.utils.data.DataLoader(mnist_trainset, batch_size=10, shuffle=True)
 mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 test_loader = torch.utils.data.DataLoader(mnist_testset, batch_size=10, shuffle=True)
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-#%%
-# 퀀타이제이션을 적용 할 기반 모델을 정의
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device
+# %%
+# 간단한 테스트용 모델 생성
 class VerySimpleNet(nn.Module):
+    # QAT 의 경우, 학습중에 Quantized activation 이 적용 된 상태로 학습을 진행하기 위해
+    # 학습전에 weight_fake_quant 레이어가 적용된 모델을 만든다.
+
     def __init__(self, hidden_size_1=100, hidden_size_2=100):
         super(VerySimpleNet,self).__init__()
+        self.quant = torch.quantization.QuantStub() # 이후 부분에 양자화 된 값을 전달하기 위한 모듈 
         self.linear1 = nn.Linear(28*28, hidden_size_1) 
         self.linear2 = nn.Linear(hidden_size_1, hidden_size_2) 
         self.linear3 = nn.Linear(hidden_size_2, 10)
         self.relu = nn.ReLU()
-
+        self.dequant = torch.quantization.DeQuantStub() # 양자화 된 값을 다시 원래 값으로 변환하기 위한 모듈
+    
     def forward(self, img):
         x = img.view(-1, 28*28)
+        x = self.quant(x) # 입력을 퀀타이제이션으로 한번 감싼다.
         x = self.relu(self.linear1(x))
         x = self.relu(self.linear2(x))
         x = self.linear3(x)
+        x = self.dequant(x)
         return x
 
 net = VerySimpleNet().to(device)
+net
 # %%
-# 모델을 학습시키는 함수 정의
+# prepare_qat 함수를 이용하여 각 Linear 레이어마다 Quantization 하도록 만듭니다.
+net.qconfig = torch.ao.quantization.default_qconfig
+net.train()
+# torch.ao.quantization.prepare_qat 레이어마다 weight_fake_quant 모듈을 붙여줍니다.
+net_quantized = torch.ao.quantization.prepare_qat(net) 
+net_quantized
+# %%
+# 학습 진행
 def train(train_loader, net, epochs=5, total_iterations_limit=None):
     cross_el = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
@@ -66,25 +78,23 @@ def train(train_loader, net, epochs=5, total_iterations_limit=None):
             data_iterator.set_postfix(loss=avg_loss)
             loss.backward()
             optimizer.step()
-
             if total_iterations_limit is not None and total_iterations >= total_iterations_limit:
                 return
 #%%
-# 모델을 학습
-train(train_loader, net, epochs=1)
-#%%
-# 모델의 크기를 확인하는 함수
+# 모델의 크기를 확인용 함수 
 def get_model_size(model):
-    torch.save(model.state_dict(), "temp_delme.p") # 임시저장
+    torch.save(model.state_dict(), "temp_delme.p")
     v = os.path.getsize("temp_delme.p")/1e3
     os.remove('temp_delme.p')
     return v
 
 def print_size_of_model(model):
-    print('모델크기(KB):', get_model_size(model))
+    print('Size (KB):', get_model_size(model))
 
 #%%
-# 모델의 Accuracy 확인 함수 정의
+# 학습을 진행합니다.
+train(train_loader, net_quantized, epochs=1)
+# %%
 def test(model: nn.Module, total_iterations: int = None):
     correct = 0
     total = 0
@@ -106,82 +116,21 @@ def test(model: nn.Module, total_iterations: int = None):
     acc = round(correct/total, 3)
     print(f'Accuracy: {acc}')
     return acc
-#%%
-# 퀀타이제이션 이전의 파라미터 값 확인
-print('퀀타이제이션 이전의 파라미터 값')
-print(net.linear1.weight)
-print(net.linear1.weight.dtype)
-print('퀀타이제이션 이전의 모델 사이즈')
-print_size_of_model(net)
-print(f'퀀타이제이션 이전의 정확도')
-test(net)
-# %%
-# 퀀타이제이션을 적용할 모델 정의
-class QuantizedVerySimpleNet(nn.Module):
-    def __init__(self, hidden_size_1=100, hidden_size_2=100):
-        super(QuantizedVerySimpleNet,self).__init__()
-        self.quant = torch.quantization.QuantStub()
-        self.linear1 = nn.Linear(28*28, hidden_size_1) 
-        self.linear2 = nn.Linear(hidden_size_1, hidden_size_2) 
-        self.linear3 = nn.Linear(hidden_size_2, 10)
-        self.relu = nn.ReLU()
-        self.dequant = torch.quantization.DeQuantStub()
 
-    def forward(self, img):
-        x = img.view(-1, 28*28)
-        x = self.quant(x)
-        x = self.relu(self.linear1(x))
-        x = self.relu(self.linear2(x))
-        x = self.linear3(x)
-        x = self.dequant(x)
-        return x
+print(f'칼리브레이션 정보 확인')
+net_quantized
 # %%
-# 퀀타이제이션을 적용할 모델 생성
-net_quantized = QuantizedVerySimpleNet().to(device)
-net_quantized.load_state_dict(net.state_dict()) # 기존 모델을 퀀타이제이션 모델에 복사
+# 수집된 칼리브레이션 정보를 이용해서 양자화를 완료합니다. 
 net_quantized.eval()
-net_quantized.qconfig = torch.ao.quantization.default_qconfig
-# 퀀타이제이션용 레이어 준비합니다. torch.ao.quantization.prepare 는 각 레이어에 MinMaxObserver 를 붙여줍니다.
-net_quantized = torch.ao.quantization.prepare(net_quantized) 
-net_quantized
-# %%
-# 테스트 배치를 통해서 activation 값을 관찰하여 min max 값을 계산함
-test(net_quantized)
-# %%
-print(f'각 레이어마다 min_value와 max_value를 확인함')
-net_quantized
-# %%
-# 퀀타이제이션을 적용. 이 과정에서 min max 값이 사용됨
-# scale과 zero_point를 계산하여 가중치와 activation을 양자화함
 net_quantized = torch.ao.quantization.convert(net_quantized)
-print(f'scale 값, zero_point 값이 계산되어 가중치와 activation이 양자화됨')
+# %%
+print(f'양자화 된 모델 정보 확인')
 net_quantized
 # %%
-# 퀀타이제이션 이후의 가중치 행렬 확인
-# torch.int_repr : https://pytorch.org/docs/stable/generated/torch.Tensor.int_repr.html
-print('퀀타이제이션 이후 가중치 행렬:')
+print('퀀타이제이션 된 모델 가중치 확인')
 print(torch.int_repr(net_quantized.linear1.weight()))
-# %%
-print('원래 가중치 확인: ')
-print(net.linear1.weight)
-#%%
-print('')
-print(f'디퀀타이즈 가중치 확인: ')
-print(torch.dequantize(net_quantized.linear1.weight()))
-print('')
-#%%
 print('Reconstruction loss: ')
 torch.mean(torch.abs(net.linear1.weight - torch.dequantize(net_quantized.linear1.weight())))
-#%%
-# Weight 히스토그램 비교
-fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-ax[0].hist(net.linear1.weight.cpu().detach().numpy().flatten(), bins=300)
-ax[1].hist(torch.int_repr(net_quantized.linear1.weight()).cpu().detach().numpy().flatten(), bins=300)
-ax[0].set_title('Original')
-ax[1].set_title('Quantized')
-plt.title('Weight histogram comparison')
-plt.legend()
-plt.show()
 # %%
 # 모델 사이즈 비교
 v1 = get_model_size(net)
